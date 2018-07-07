083c1ae53cf8052eb462da298e7b8662bbb2b4b3
wisonlau <122022066@qq.com>
2018-07-07 14:41:23 +0800

    Update Collection.php

--- src/Collection.php
+++ src/Collection.php
@@ -26,7 +26,7 @@ interface Jsonable
     public function toJson($options = 0);
 }
 
-class Collection
+class Collection implements Arrayable,Jsonable
 {
     /**
      * The items contained in the collection.
